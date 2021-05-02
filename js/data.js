3ee553407cea1d9dd64a9071e9ed7122f401cf79
mulyasaputra <maskleaderteam@gmail.com>
2021-05-02 14:11:34 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,17 @@
+$.getJSON('data/website.json', function (data) { let website = data.website;
+   $.each(website, function (i, data) {
+      $('#website').append('<div class="container"><a href="'+ data.link +'"><img src="img/'+ data.img +'" alt="Payment" class="image"><div class="overlay"><div class="text">'+ data.start +'<span>'+ data.end +'</span></div></div></a></div>')
+   });
+});
+
+$.getJSON('data/design.json', function (data) { let design = data.design;
+   $.each(design, function (i, data) {
+      $('#design').append('<div class="container"><a href="'+ data.link +'"><img src="img/'+ data.img +'" alt="Payment" class="image"><div class="overlay"><div class="text">'+ data.start +'<span>'+ data.end +'</span></div></div></a></div>')
+   });
+});
+
+$.getJSON('data/apps.json', function (data) { let apps = data.apps;
+   $.each(apps, function (i, data) {
+      $('#apps').append('<div class="container"><a href="'+ data.href +'"><img src="img/'+ data.pic +'" alt="Payment" class="image"><div class="overlay"><div class="text">'+ data.fres +'<span>'+ data.spn +'</span></div></div></a></div>')
+   });
+});
