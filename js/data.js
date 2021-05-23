a18acaf31ae62a072873c53f254aa50d013aa6f9
mulyasaputra <maskleaderteam@gmail.com>
2021-05-23 11:04:50 +0700

    add responsive

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ $.getJSON("data/website.json", function (data) {
   let website = data.website;
   $.each(website, function (i, data) {
     $("#website").append(
-      '<div class="container"><a target="_blank" href="' +
+      '<div class="container" data-aos="fade-up-right"><a target="_blank" href="' +
         data.link +
         '"><img src="img/' +
         data.img +
@@ -19,7 +19,7 @@ $.getJSON("data/design.json", function (data) {
   let design = data.design;
   $.each(design, function (i, data) {
     $("#design").append(
-      '<div class="container"><a target="_blank" href="' +
+      '<div class="container" data-aos="fade-left"><a target="_blank" href="' +
         data.link +
         '"><img src="img/' +
         data.img +
@@ -36,7 +36,7 @@ $.getJSON("data/apps.json", function (data) {
   let apps = data.apps;
   $.each(apps, function (i, data) {
     $("#apps").append(
-      '<div class="container"><a target="_blank" href="' +
+      '<div class="container" data-aos="zoom-in-up"><a target="_blank" href="' +
         data.href +
         '"><img src="img/' +
         data.pic +
