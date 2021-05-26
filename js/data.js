4837e7f698f5138bf06c490c746dbbe2e9419d43
mulyasaputra <maskleaderteam@gmail.com>
2021-05-26 10:23:05 +0700

    fix update2

--- js/data.js
+++ js/data.js
@@ -19,7 +19,7 @@ $.getJSON("data/design.json", function (data) {
   let design = data.design;
   $.each(design, function (i, data) {
     $("#design").append(
-      '<div class="container" data-aos="fade-left"><a target="_blank" href="' +
+      '<div class="container" data-aos="fade-right"><a target="_blank" href="' +
         data.link +
         '"><img src="img/' +
         data.img +
