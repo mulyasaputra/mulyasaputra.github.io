83624b33dfba675d08400b9db187f127db6859f4
mulyasaputra <maskleaderteam@gmail.com>
2021-08-07 21:52:37 +0700

    Fix For PotatoScreen

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,17 @@
+var width = screen.width;
 $(document).ready(function () {
+  $(window).scroll(function () {
+    // PotatoScreen
+    if (width < 360) {
+      alert(
+        "Maaf ukuran layar hp anda terlalu kentang. \n Web ini belum sempurna untu ukuran layar kurang dari 360px, \n Buka dalam mode desktop atau ganti dengan hp yang memiliki \n ukuran layar lebih besar dari yang kau pegang sekarang"
+      );
+      if (alert) {
+        width = 361;
+      }
+    }
+  });
+
   $(".menu .daftar a").click(function () {
     // applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
