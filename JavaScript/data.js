479df42ddf4caf3f07db6b708c74fbc5cc7dca2a
mulyasaputra <maskleaderteam@gmail.com>
2021-08-10 20:00:58 +0700

    Web server

--- JavaScript/data.js
+++ JavaScript/data.js
@@ -1,25 +1,19 @@
-// Get the modal
-$("#inText").keydown(function () {});
-var klik = document.getElementById("tombol");
-var warning = "Jangan lupa Tulis Pesan sebelim di kirim";
-
-klik.onclick = function () {
-  if ($("#inText").val() == "") {
-    alert(warning);
-  } else {
-    var pesan = document.getElementById("inText").value;
-    window.location =
-      "https://api.whatsapp.com/send?phone=6281542253290&text=" + pesan;
-  }
-};
-
-// View Tahun
-date = new Date();
-tahun = date.getFullYear();
-var hakCipta =
-  "Hak cipta" +
-  " © " +
-  tahun +
-  " InSketch. Seluruh hak cipta dilindungi oleh siapa.";
-
-document.getElementById("cc").innerHTML = hakCipta;
+//  Script for Data
+$.getJSON("Json/Thumbnail.json", function (data) {
+  let design = data.design;
+  $.each(design, function (i, data) {
+    $("#lembar").append(
+      '<div class="card"><img src="' +
+        data.gambar +
+        '" alt="' +
+        data.alt +
+        '" /><div class="label"><a target="_blank" href="' +
+        data.link +
+        '"><h3>' +
+        data.nama +
+        '</h3><i class="fas fa-arrow-right"></i></a><p>' +
+        data.paraf +
+        "</p></div></div>"
+    );
+  });
+});
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,4 @@
+// Index Respon JavaScript
 var width = screen.width;
 $(document).ready(function () {
   $(window).scroll(function () {
@@ -23,3 +24,30 @@ $(document).ready(function () {
     $(".menu-btn i").toggleClass("active");
   });
 });
+
+// Index Model JavaScript
+// Get the modal
+$("#inText").keydown(function () {});
+var klik = document.getElementById("tombol");
+var warning = "Jangan lupa Tulis Pesan sebelim di kirim";
+
+klik.onclick = function () {
+  if ($("#inText").val() == "") {
+    alert(warning);
+  } else {
+    var pesan = document.getElementById("inText").value;
+    window.location =
+      "https://api.whatsapp.com/send?phone=6281542253290&text=" + pesan;
+  }
+};
+
+// View Tahun
+date = new Date();
+tahun = date.getFullYear();
+var hakCipta =
+  "Hak cipta" +
+  " © " +
+  tahun +
+  " InSketch. Seluruh hak cipta dilindungi oleh siapa.";
+
+document.getElementById("cc").innerHTML = hakCipta;
