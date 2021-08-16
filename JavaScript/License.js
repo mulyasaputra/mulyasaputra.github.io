d911badf448ce6e9f76c5ae6e0d740a5e7975e3d
mulyasaputra <maskleaderteam@gmail.com>
2021-08-16 20:37:08 +0700

    update

--- JavaScript/License.js
+++ JavaScript/License.js
@@ -1,25 +1,28 @@
 function Hilangarah() {
   $(".License, .Privacy, .Terms, .API").addClass("hilang");
-  //   $("#Btn1, Btn2, Btn3, #Btn4").addClass("hilang");
+  $("#Btn1, #Btn2, #Btn3, #Btn4").addClass("Tambah");
 }
+$("#Btn2, #Btn3, #Btn4").addClass("Tambah");
 
 // Button onClick
 $("#Btn1").on("click", function () {
   Hilangarah();
   $(".License").removeClass("hilang");
+  $("#Btn1").removeClass("Tambah");
 });
 
 $("#Btn2").on("click", function () {
   Hilangarah();
   $(".Privacy").removeClass("hilang");
+  $("#Btn2").removeClass("Tambah");
 });
 
 $("#Btn3").on("click", function () {
   Hilangarah();
   $(".Terms").removeClass("hilang");
+  $("#Btn3").removeClass("Tambah");
 });
 
 $("#Btn4").on("click", function () {
-  Hilangarah();
-  $(".API").removeClass("hilang");
+  window.open("https://mulyasaputra.github.io/visite/", "_blank");
 });
--- JavaScript/universal.js
+++ JavaScript/universal.js
@@ -1,18 +1,16 @@
-// Index Respon JavaScript
+// PotatoScreen
 var width = screen.width;
-$(document).ready(function () {
-  $(window).scroll(function () {
-    // PotatoScreen
-    if (width < 308) {
-      alert(
-        "Maaf ukuran layar hp anda terlalu kentang. \n Web ini belum sempurna untu ukuran layar kurang dari 360px, \n Buka dalam mode desktop atau ganti dengan hp yang memiliki \n ukuran layar lebih besar dari yang kau pegang sekarang"
-      );
-      if (alert) {
-        width = 361;
-      }
-    }
-  });
+if (width < 308) {
+  alert(
+    "Maaf ukuran layar hp anda terlalu kentang. \n Web ini belum sempurna untu ukuran layar kurang dari 360px, \n Buka dalam mode desktop atau ganti dengan hp yang memiliki \n ukuran layar lebih besar dari yang kau pegang sekarang"
+  );
+  if (alert) {
+    width = 361;
+  }
+}
 
+// Index Respon JavaScript
+$(document).ready(function () {
   $(".menu .daftar a").click(function () {
     // applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
