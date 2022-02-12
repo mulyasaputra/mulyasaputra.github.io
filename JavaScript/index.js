60ab4cb1118ba79d4570841867a667a13cdf9eef
mulyasaputra <maskleaderteam@gmail.com>
2022-02-12 23:43:30 +0700

    Update Fix 2.2.3

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -19,13 +19,19 @@ $.getJSON("https://inskrtch-de394-default-rtdb.firebaseio.com/index.json?print=p
   let postVideo = data.postVideo;
   $.each(postVideo, function (i, data) {
     $("#embed").append(
-      '<div class="card-video"><div class="videos"><img src="' +
-        data.urlGambar +
-        '"/><div class="play"><a href="#"><img src="/Img/play-circle-solid.svg" /></a></div></div><div class="title-video"><i class="fas fa-share"></i><a href="' +
-        data.urlVideo +
-        '">' +
-        data.urlNama +
-        "</a></div></div>"
+      `<div class="card-video">
+        <div class="videos">
+          <img
+            src="${data.urlGambar}"
+          />
+          <div class="play">
+            <a href="${data.urlVideo}" id="videolink" class="button more">
+              <img src="/Img/play-circle-solid.svg" />
+            </a>
+          </div>
+        </div>
+        <div class="title-video"><i class="fas fa-share"></i><a href="${data.urlVideo}">${data.urlNama}</a></div>
+      </div>`
     );
   });
 });
@@ -54,3 +60,24 @@ klik.onclick = function () {
 //   $(".menuLink .LApps").removeClass("active");
 //   $(" .menuLink .apps").removeClass("active");
 // }
+
+$("#videolink").magnificPopup({
+  type: "inline",
+  midClick: true,
+  closeOnContentClick: true,
+  showCloseBtn: false,
+});
+
+{
+  /* <div id="${data.idNama}" class="mfp-hide">
+<iframe
+  width="853"
+  height="480"
+  src="https://www.youtube.com/embed/${data.urlVideo}"
+  title="${data.urlNama}"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+></iframe>
+</div> */
+}
